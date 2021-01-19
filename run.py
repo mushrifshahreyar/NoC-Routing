5f2fde24272d4623a79ab57bd30826c4d90228b0
Mushrif <mushrifshahreyar@gmail.com>
2021-01-19 12:10:07 +0530

    Trained

--- run.py
+++ run.py
@@ -1,19 +1,26 @@
 import os
+import progressbar
+import time
+import subprocess
 
-ITERATIONS = 3
-i = 0
+ITERATIONS = 1000
+#i = 0
+
+print("Deleting existing Q-Table")
 os.system('rm Q_Table.txt')
 return_code = os.system('scons ./build/X86/gem5.opt')
 
 
 
 if return_code == 0:
-	print("Starting Training Phase")
-	while i < ITERATIONS:
-		os.system('./build/X86/gem5.opt -d m5out/ configs/example/garnet_synth_traffic.py --num-cpus=16 --num-dirs=16 --network=garnet --topology=Mesh_XY --mesh-rows=4 --sim-cycles=100000 --inj-vnet=-1 --vcs-per-vnet=8 --injectionrate=0.1 --synthetic=uniform_random   --routing-algorithm=3')
+    print("Starting Training Phase")
+    bar = progressbar.ProgressBar(maxval=ITERATIONS, widgets = [' [', progressbar.Timer(), '] ', progressbar.Bar('=', ' [','] '), ' ', progressbar.Percentage()]).start()
 
-		i += 1
-		print("Training Phase Ended")
+    for i in range(ITERATIONS):
+        bar.update(i)
+        result = os.system('./build/X86/gem5.opt -d m5out/ configs/example/garnet_synth_traffic.py --num-cpus=16 --num-dirs=16 --network=garnet --topology=Mesh_XY --mesh-rows=4 --sim-cycles=100000 --inj-vnet=-1 --vcs-per-vnet=8 --injectionrate=0.1 --synthetic=uniform_random   --routing-algorithm=3 > /dev/null 2>&1')
+
+    print("Training Phase Ended")
 
 else:
-	print('Error occured while compiling!')
+    print('Error occured while compiling!')
